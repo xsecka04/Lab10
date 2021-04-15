cb1445e62c211c6fcc262fd5c0f6b8266bcacf36
Martin Sečkar <m.seckar@hotmail.com>
2021-04-15 13:06:11 +0200

    Add project files.

---
+++ InsertQueries.sql
@@ -0,0 +1,18 @@
+﻿INSERT INTO Student VALUES ('2001', 'Jan', 'Novak', '1992-2-10');
+INSERT INTO Student VALUES ('2002', 'Peter', 'Novak', '1992-2-10');
+INSERT INTO Student VALUES ('2003', 'Jan', 'Novakovich', '1992-2-10');
+INSERT INTO Student VALUES ('2004', 'Peter', 'Novakovich', '1992-2-10');
+
+INSERT INTO Predmet VALUES ('BPC-OOP', 'Objektove Orientovane Programovani');
+INSERT INTO Predmet VALUES ('BPC-TIN', 'Teoreticka Informatika');
+INSERT INTO Predmet VALUES ('BPC-SOS', 'Operacni Systemy');
+
+INSERT INTO Zapsane(Skratka, StudentId) VALUES ('BPC-OOP', '2001');
+INSERT INTO Zapsane(Skratka, StudentId) VALUES ('BPC-TIN', '2003');
+INSERT INTO Zapsane(Skratka, StudentId) VALUES ('BPC-SOS', '2002');
+INSERT INTO Zapsane(Skratka, StudentId) VALUES ('BPC-SOS', '2001');
+
+INSERT INTO Hodnoceni(Skratka, StudentId, DatumHodnoceni, Hodnoceni) VALUES ('BPC-OOP', '2001', GETDATE(), 1);
+INSERT INTO Hodnoceni(Skratka, StudentId, DatumHodnoceni, Hodnoceni) VALUES ('BPC-SOS', '2002', GETDATE(), 2);
+INSERT INTO Hodnoceni(Skratka, StudentId, DatumHodnoceni, Hodnoceni) VALUES ('BPC-TIN', '2003', GETDATE(), 3);
+INSERT INTO Hodnoceni(Skratka, StudentId, DatumHodnoceni, Hodnoceni) VALUES ('BPC-SOS', '2001', GETDATE(), 1);
---
+++ SelectQueries.sql
@@ -0,0 +1,19 @@
+﻿SELECT Student.Jmeno, Student.Prijmeni, Zapsane.Skratka
+FROM Student
+LEFT JOIN Zapsane ON Student.Id = Zapsane.StudentId
+ORDER BY Student.Prijmeni;
+
+SELECT COUNT(Prijmeni), Prijmeni
+FROM Student
+GROUP BY Prijmeni
+ORDER BY COUNT(Prijmeni) DESC;
+
+SELECT COUNT(Skratka), Skratka
+FROM Zapsane
+GROUP BY Skratka
+HAVING COUNT(Skratka)<3;
+
+SELECT Predmet.Nazev, Hodnoceni.Hodnoceni
+From Predmet
+LEFT JOIN Hodnoceni ON Predmet.Skratka = Hodnoceni.Skratka
+GROUP BY Predmet.Nazev;
